21a52cad334057f8fb0e2a4b3583a5afa4e4183b
brkybzkrt <b9bozkurt959@gmail.com>
2021-09-05 09:58:44 +0300

    added express, mongoose, handlebars, nodemon  and body-parser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require('express')
 const exphbs  = require('express-handlebars')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
 const app= express()
 const port=3000
@@ -13,16 +15,22 @@ mongoose.connect('mongodb://localhost/blog_app');
 app.use(express.static('public'))
 
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+    })
+);
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: false }))
- 
-
 app.use(bodyParser.json())
 
 const main = require('./routes/main')
 app.use('/',main)
+
+
+const post = require('./routes/posts')
+app.use('/posts',post)
 
 
 app.listen(port,hostName,()=>{

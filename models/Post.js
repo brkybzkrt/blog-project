ac6b7460635d398cbed6fe259fa150e4d1d5cb14
brkybzkrt <b9bozkurt959@gmail.com>
2021-09-08 20:15:50 +0300

    created Category and Post models

--- models/Post.js
+++ models/Post.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const PostSchema= new mongoose.Schema({
 
     title:{ type:String,required:true},
+    author:{type:mongoose.Schema.Types.ObjectId,ref:'users'},
     content:{ type:String,required:true},
+    category:{type:mongoose.Schema.Types.ObjectId,ref:'categories'},
     date:{ type:Date,default:Date.now},
     post_image:{ type:String,required:true}
+    
 })
 
 module.exports= mongoose.model('Post',PostSchema)

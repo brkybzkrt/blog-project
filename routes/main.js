b956a79366c7396f2b2375891bb46afec8e79f57
brkybzkrt <b9bozkurt959@gmail.com>
2021-09-11 11:31:54 +0300

    updated routes

--- routes/main.js
+++ routes/main.js
@@ -1,18 +1,40 @@
 const express=require('express')
 const router = express.Router()
 const Post= require('../models/Post')
+const Category= require('../models/Category')
 
 router.get('/',(req,res)=>{
 
     res.render('site/index')
-    
+   
+  
 })
 
 
 
-router.get('/blogs',(req,res)=>{
-    Post.find({}).sort({_id:-1}).then(posts=>{
-        res.render('site/blogs',{posts:posts})
+router.get('/posts',(req,res)=>{
+    Post.find({}).populate({path:'author',model:'User'}).sort({_id:-1}).then(posts=>{
+        Category.aggregate([{
+
+            $lookup:{
+                from:'posts',
+                localField:'_id',
+                foreignField:'category',
+                as:'posts'
+            }},
+
+            {$project:{
+                _id:1,
+                name:1,
+                countOfPosts:{$size: '$posts'}
+            }}
+        ]).sort({_id:-1}).then(categories=>{
+            Post.find({}).sort({_id:-1}).limit(3).then(threepost=>{
+                res.render('site/posts',{posts,categories,threepost})
+            })
+           
+        })
+        
     })
 
 })
--- routes/posts.js
+++ routes/posts.js
@@ -2,25 +2,85 @@ const express=require('express')
 const router = express.Router()
 const Post= require('../models/Post')
 const path= require('path')
+const Category = require('../models/Category')
+
 
 
 
 router.get('/new',(req,res)=>{
     
-    if(req.session.userId){
-        res.render('site/addpost')
+    if(!req.session.userId){
+        res.redirect('/users/login')
     }
     else{
-        res.redirect('/users/login')
+        Category.find({}).sort({_id:-1}).then(categories=>{
+
+            res.render('site/addpost',{categories})
+        })
     }
    
 })
 
+router.get('/category/:categoryId',(req,res)=>{
+let categoryId=req.params.categoryId
+Post.find({category:categoryId}).sort({_id:-1}).then(posts=>{
+    Category.aggregate([{
+
+        $lookup:{
+            from:'posts',
+            localField:'_id',
+            foreignField:'category',
+            as:'posts'
+        }},
+
+        {$project:{
+            _id:1,
+            name:1,
+            countOfPosts:{$size: '$posts'}
+        }}
+    ]).sort({_id:-1}).then(categories=>{
+        Post.find({}).sort({_id:-1}).limit(3).then(threepost=>{
+            res.render('site/posts',{posts,categories,threepost})
+        })
+       
+    })
+    
+   
+})
+
+
+})
+
+
 router.get('/:id',(req,res)=>{
-Post.findById(req.params.id).then(post=>
-    {
-    res.render('site/blog-single',{post:post})
-})}) 
+Post.findById(req.params.id).populate({path:'author',model:'User'}).then(post=>
+    Category.aggregate([{
+
+        $lookup:{
+            from:'posts',
+            localField:'_id',
+            foreignField:'category',
+            as:'posts'
+        }},
+
+        {$project:{
+            _id:1,
+            name:1,
+            countOfPosts:{$size: '$posts'}
+        }}
+    ]).sort({_id:-1}).then(categories=>{
+        {
+            Post.find({}).sort({_id:-1}).limit(3).then(threepost=>{
+                res.render('site/post-single',{post,categories,threepost})
+            })
+            
+        }
+    })
+   
+)}) 
+
+
+
 
 
 router.post('/test',(req,res)=>{
@@ -30,15 +90,18 @@ router.post('/test',(req,res)=>{
     post_image.mv(path.resolve(__dirname,'../public/img/postImages/',post_image.name))
     Post.create({
         ...req.body,
-        post_image:`/img/postImages/${post_image.name}`
+        post_image:`/img/postImages/${post_image.name}`,
+        author:req.session.userId
 
     })
     req.session.sessionFlash ={
         type:'alert alert-success',
         message:'Post başarıyla kaydedildi.'
     }
-    res.redirect('/blogs')
+    res.redirect('/posts')
 })
+
+
 
 
 module.exports=router
